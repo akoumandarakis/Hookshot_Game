a98ab98f9e89e9e452a172abb0d8784036079842
akoumandarakis <kalphabet111@gmail.com>
2016-09-23 10:29:26 -0600

    Created a Weapon Prefab
    Created the weapon prefab that follows the mouse of the player, as well
    as a shot prefab and a basic movement script for the shot. I used
    stand-in assets from the unity tutorial I did.

---
+++ Hookshot/Assets/Scripts/ShotScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Defines the behavior of a shot
+/// </summary>
+public class ShotScript : MonoBehaviour {
+
+    /// <summary>
+    /// The damage the shot can do
+    /// </summary>
+    public int damage = 1;
+    /// <summary>
+    /// Whether or not that shot is an enemy's
+    /// </summary>
+    public bool enemyShot = false;
+
+	// Use this for initialization
+	void Start () {
+        //Destroy any shot after ten seconds
+        Destroy(gameObject, 10);
+	}
+}
---
+++ Hookshot/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Creates a weapon that can generate shots
+/// </summary>
+public class WeaponScript : MonoBehaviour {
+
+    /// <summary>
+    /// The shot that will be fired by the weapon
+    /// </summary>
+    public Transform shotPrefab;
+
+    /// <summary>
+    /// The rate at which the weapon can fire
+    /// </summary>
+    public float shotRate;
+
+    /// <summary>
+    /// Whether or not the weapon is the players' or an enemy's
+    /// </summary>
+    public bool enemyWeapon;
+
+    /// <summary>
+    /// The time until the next shot can be fired
+    /// </summary>
+    private float shotCooldown;
+
+    /// <summary>
+    /// The position of the mouse
+    /// </summary>
+    private Vector3 mousePos;
+
+    /// <summary>
+    /// The position of the screen
+    /// </summary>
+    private Vector3 screenPos;
+
+	// Use this for initialization
+	void Start () {
+        shotCooldown = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Update the rotation of the weapon
+        updateRotation();
+
+        //Count down until next shot
+        if (shotCooldown > 0)
+        {
+            shotCooldown -= Time.deltaTime;
+        }
+        
+        //Enemy weapons just shoot continuously
+        if (enemyWeapon)
+        {
+            Shoot(true);
+        }
+        
+        //If the left mouse button is clicked and it's a player's weapon, fire the weapon 
+        if (Input.GetButtonDown("Fire1") && !enemyWeapon)
+        {
+            Shoot(false);
+        }
+	}
+
+    /// <summary>
+    /// Creates a shot based on the position and rotation of the weapon
+    /// </summary>
+    /// <param name="isEnemy">Whether or not the shot is an enemy's shot</param>
+    public void Shoot(bool isEnemy)
+    {
+        //If the shot cooldown has reached 0
+        if (shotCooldown <= 0f)
+        {
+            //Reset the shot cooldown
+            shotCooldown = shotRate;
+
+            //Create a new shot at the position of the weapon
+            var shot = Instantiate(shotPrefab) as Transform;
+            shot.position = transform.position;
+
+            ShotScript shotScript = shot.gameObject.GetComponent<ShotScript>();
+
+            //Sets whether or not the shot is an enemy's or the player's
+            if (shot != null)
+            {
+                 shotScript.enemyShot = isEnemy;
+            }
+
+            //Makes sure the shot moves right relative to the weapon
+            MoveScript movement = shot.gameObject.GetComponent<MoveScript>();
+            if (movement != null)
+            {
+                movement.direction = this.transform.right;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates the rotation of the player's weapon based on the position of the mouse
+    /// </summary>
+    public void updateRotation()
+    {
+        if (!enemyWeapon)
+        {
+            //Gets the position of the mouse and creates a vector from the that position on the screen
+            mousePos = Input.mousePosition;
+            screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - Camera.main.transform.position.z));
+
+            //Sets the rotation of the weapon based on that position
+            transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((screenPos.y - transform.position.y), (screenPos.x - transform.position.x)) * Mathf.Rad2Deg);
+        }
+    }
+}
