d804ba3dc5a61c12aa4dcccbde368afc929690f0
Austen <u0941256@kcmac-05.scl.utah.edu>
2016-10-17 17:52:33 -0600

    Added weapon and turrets to level

--- Hookshot/Assets/Scripts/WeaponScript.cs
+++ Hookshot/Assets/Scripts/WeaponScript.cs
@@ -90,7 +90,7 @@ public class WeaponScript : MonoBehaviour
 
             //Create a new shot at the position of the weapon
 			var shot = Instantiate(shotPrefab) as Transform;
-            shot.position = new Vector3(transform.position.x + transform.right.x, transform.position.y + transform.right.y, transform.position.z);
+            shot.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             shot.eulerAngles = transform.eulerAngles;
 
             ShotScript shotScript = shot.gameObject.GetComponent<ShotScript>();
