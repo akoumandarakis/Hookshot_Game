bceef118e709a59b42d85f545feb65875fc046cb
Nate Shirey <nateshirey@Nates-MacBook-Pro.local>
2016-10-21 00:09:49 -0600

    Moved some textures, added keycards, altered first section of level sprites, Implemented a layer system, slightly lowered character jump height (more for testing level height than anything so change it back if you feel it was right before).

---
+++ Hookshot/Assets/Prefabs/KeyCards/Card_Glow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card_Glow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	float opaque = 1.0f;
+
+	float alpha = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		
+		lightPulse ();
+
+	}
+
+	void lightPulse (){
+
+		float brightness = Mathf.PingPong (Time.time, opaque);
+
+
+		alpha = Mathf.Lerp (0.2f, 0.8f, brightness);
+		SpriteRenderer rend = GetComponent <SpriteRenderer> ();
+		Color _color = rend.color;
+		_color.a = alpha;
+		rend.color = _color;
+	
+
+	}
+
+}
+
