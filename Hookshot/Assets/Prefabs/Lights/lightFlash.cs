cab18da9831d9f18b574b42ad945d5b06ae52074
Nate Shirey <nateshirey@Nates-MacBook-Pro.local>
2016-10-23 21:30:16 -0600

    Added prefabs for details, lights, edited prefabs for environment. Added particle effect. Added keycard prefab. modified texture atlas. Began to implement visibility concealment.

---
+++ Hookshot/Assets/Prefabs/Lights/lightFlash.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightFlash : MonoBehaviour {
+	public float speed;
+	public float brightness;
+
+	Renderer rend; 
+
+	Color _tempColor;
+
+	int direction = 1;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (rend.material.color.a >= brightness || rend.material.color.a <= 0.1f) {
+
+			direction = direction * -1;
+
+		}
+		_tempColor = rend.material.color;
+		_tempColor.a = _tempColor.a + (direction * (_tempColor.a * (speed * Time.deltaTime)));
+		rend.material.color = _tempColor;
+	
+	}
+}
