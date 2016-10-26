e2efda8ae8787db446483cb0efedd7b5da7c2b3b
Nate Shirey <nateshirey@nates-mbp.uconnect.utah.edu>
2016-10-26 09:11:33 -0600

    hopefully fixes the differences in scenes and prefabs for background objects

--- Hookshot/Assets/Scripts/VeilAlphaScript.cs
+++ Hookshot/Assets/Scripts/VeilAlphaScript.cs
@@ -3,36 +3,61 @@ using System.Collections;
 
 public class VeilAlphaScript : MonoBehaviour {
 
+	SpriteRenderer rend;
+	float lerpSpeed = 1.0f;
+	float _alpha = 0.0f;
+	public float startTime = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		rend = GetComponent <SpriteRenderer> ();
+	
 	}
-
-
-
-	float lerpSpeed = 1.0f;
 
 	// Update is called once per frame
 	void Update () {
 		
 
-		
-		}
+	}
+	void OnTriggerStay2d (Collision2D col){
+	
+		Debug.Log ("enter");
 
 	}
-
 	void OnCollisionEnter2D (Collision2D Coll) {
 
-		if(Coll.gameObject.tag == "Player"); {
+		startTime = Time.time;
+		Debug.Log ("enter");
 
 
-			SpriteRenderer rend = GetComponent <SpriteRenderer> ();
-			Color _tempcolor = rend.color;
-			_tempcolor.a = Mathf.Lerp (1.0f, 0.0f,Time.deltaTime);
-			rend.color = _tempcolor;
+	}
+	void OnCollisionStay2D (Collision2D Coll) {
+		Debug.Log ("stay");
+		if(Coll.gameObject.tag == "Player") {
 
+			if (rend.color.a >= 0.0f) {
+
+				Color _tempcolor = rend.color;
+				_tempcolor.a = _tempcolor.a - Time.deltaTime;
+
+				//Color _tempcolor = rend.color;
+				//Debug.Log (_tempcolor);
+				//StartCoroutine ("Fade");
+
+
+			}
 
 		}
+			
+	}
 
+	public IEnumerator Fade (){
+		Debug.Log ("in coroutine");
+		Color _tempcolor = rend.color;
+
+		_tempcolor.a = Mathf.SmoothStep (_tempcolor.a, 0.0f, Time.time - startTime);
+		rend.color = _tempcolor;
+		yield return _tempcolor.a;
 	}
 }
