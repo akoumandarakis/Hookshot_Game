0510bdfa94201284255c11d0210667b37e27a762
Alex Koumandarakis <u0910745@kcmac-79.scl.utah.edu>
2016-11-10 11:24:06 -0700

    Missile now accelerates towards the player rather than just going straight to the player's position.

--- Hookshot/Assets/Scripts/MoveTowardScript.cs
+++ Hookshot/Assets/Scripts/MoveTowardScript.cs
@@ -8,38 +8,45 @@ public class MoveTowardScript : MonoBehaviour {
 	/// </summary>
 	public Vector2 acceleration;
 
+	public float accelerationConstant;
+
 	/// <summary>
 	/// The direction that the object will move in
 	/// </summary>
 	private Vector2 direction;
 
 	/// <summary>
-	/// The speed of the object
-	/// </summary>
-	public Vector2 speed = new Vector2(0f,0f);
-
-	/// <summary>
 	/// The speed multiplied by the direction
 	/// </summary>
-	private Vector2 velocity;
+	public Vector2 velocity = new Vector2(0f, 0f);
 
 	public GameObject objectToMoveTowards;
 
 	// Update is called once per frame
 	void Update () {
-		speed = new Vector2(speed.x + acceleration.x, speed.y + acceleration.y);
-
-		//Gets the position of the object
+		//Gets the position of the object to accelerate towards
 		Vector3 objPos = objectToMoveTowards.transform.position;
 
-		//Sets the rotation of the weapon based on that position
-		transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((objPos.y - transform.position.y), (objPos.x - transform.position.x)) * Mathf.Rad2Deg);
+		//The distance in the x and y axises from the object to chase to this object
+		float yDif = objPos.y - transform.position.y;
+		float xDif = objPos.x - transform.position.x;
 
-		direction = this.transform.right;
+		//The angle of the vector (in RADIANS) pointing from this object to the object to chase
+		float angleOfDirectionVector = Mathf.Atan2 (yDif, xDif);
 
-		velocity = new Vector3 (speed.x * direction.x, speed.y * direction.y, 0);
+		//Set the direction
+		direction = new Vector2 (Mathf.Cos (angleOfDirectionVector), Mathf.Sin(angleOfDirectionVector));
 
-		velocity *= Time.deltaTime;
+		//Set the acceleration vector 
+		acceleration = new Vector2(accelerationConstant * direction.x, accelerationConstant * direction.y);
+
+		acceleration *= Time.deltaTime;
+
+		velocity = new Vector2(velocity.x + acceleration.x, velocity.y + acceleration.y);
+
+		transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg);
+
+		//velocity *= Time.deltaTime;
 		transform.Translate(velocity, Space.World);
 	}
 }
