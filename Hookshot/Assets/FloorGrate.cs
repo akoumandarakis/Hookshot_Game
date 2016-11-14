a7680e84ca43fc7f80bbd0152dbebbc59b607804
Nate Shirey <nateshirey@Nates-MacBook-Pro.local>
2016-11-13 18:31:48 -0700

    Worked on lights, Blood Splatters, and Secret passage prefabs. Built out more level stuff.

---
+++ Hookshot/Assets/FloorGrate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorGrate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollision2DEnter(Collision2D coll) {
+		Rigidbody2D rigid = gameObject.GetComponent<Rigidbody2D> ();
+		rigid.gravityScale = 1.0f;
+	}
+}
---
+++ Hookshot/Assets/LightColorPick.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightColorPick : MonoBehaviour {
+	public bool Red;
+	public bool Green;
+	public bool Blue;
+	public bool LightBlue;
+	public bool Purple;
+	public bool White;
+	SpriteRenderer Rend;
+	// Use this for initialization
+	void Start () {
+		Rend = gameObject.GetComponent<SpriteRenderer> ();
+		if (Red == true){
+			Rend.color = new Color (.88f, 0.066f, 0.082f, 1.0f);
+
+		}
+		if (Green == true){
+			Rend.color = new Color (.208f, 0.972f, 0.494f, 1.0f);
+
+		}
+		if (Blue == true){
+			Rend.color = new Color (.09f, 0.074f, 0.776f, 1.0f);
+
+		}
+		if (LightBlue == true){
+			Rend.color = new Color (.157f, 0.898f, 0.925f, 1.0f);
+
+		}
+		if (Purple == true){
+			Rend.color = new Color (.694f, 0.129f, 0.878f, 1.0f);
+
+		}
+		if (White == true){
+			Rend.color = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
