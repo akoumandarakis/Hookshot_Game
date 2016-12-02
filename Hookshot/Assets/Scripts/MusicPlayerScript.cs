446289523ba2e05898ed32ba1447cd2b163f87d1
Alex Koumandarakis <u0910745@kcmac-04.scl.utah.edu>
2016-12-01 18:05:13 -0700

    Music player now has script that allows it to take a track to be looped.

---
+++ Hookshot/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+	public AudioClip loopClip;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		AudioSource audio = GetComponent<AudioSource>();
+
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		if (loopClip != null) {
+			audio.clip = loopClip;
+			audio.Play ();
+		}
+	}
+}
