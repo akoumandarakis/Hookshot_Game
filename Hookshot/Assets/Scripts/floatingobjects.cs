43fe0948a17433fc80cd386381af60c7790b6614
Nate Shirey <nateshirey6@gmail.com>
2016-12-04 13:24:18 -0700

    Zero G room setting up

---
+++ Hookshot/Assets/Scripts/floatingobjects.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floatingobjects : MonoBehaviour {
+
+	private float currentPosition;
+
+	public float interval;
+
+	public float maxDistance;
+
+
+	// Use this for initialization
+	void Start () {
+
+		currentPosition = 0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (zeroG) {
+			if (currentPosition <= maxDistance) {
+
+				currentPosition = Mathf.MoveTowards (currentPosition, maxDistance, (interval * Time.deltaTime));
+
+				this.transform.localPosition = new Vector3 (0f, currentPosition, 0f);
+		
+			}
+		//}
+		//if(notzeroG && currentPosition >= 0f){
+
+			//currentPosition = Mathf.MoveTowards (currentPosition, 0f, (interval * Time.deltaTime));
+
+			//this.transform.localPosition = new Vector3 (0f, currentPosition, 0f);
+
+		//}
+	}
+}
