8a7208c28e90e0a55be3699679131536d64259f5
Nate Shirey <nateshirey6@gmail.com>
2016-12-05 17:49:47 -0700

    zero g switch animation and prefab change

--- Hookshot/Assets/Scripts/ZeroGSwitchScript.cs
+++ Hookshot/Assets/Scripts/ZeroGSwitchScript.cs
@@ -9,6 +9,13 @@ public class ZeroGSwitchScript : MonoBehaviour {
 
 	private HookShotScript hookshot;
 	private PlayerScript player;
+	private AnimationController2D anim;
+	private int switchState = 0;
+
+	void Start()
+	{
+		anim = this.gameObject.GetComponent<AnimationController2D>();
+	}
 
 	void OnTriggerEnter2D (Collider2D collider)
 	{
@@ -30,16 +37,20 @@ public class ZeroGSwitchScript : MonoBehaviour {
 			if (Input.GetButtonDown ("Button Press")) {
 				if (ZeroGArea.layer == 0) {
 					ZeroGArea.layer = LayerMask.NameToLayer ("Trigger");
+					anim.setAnimation ("Zero G off to on");
 				} else if (ZeroGArea.layer == 10) {
+					anim.setAnimation ("zero g on to off");
 					ZeroGArea.layer = LayerMask.NameToLayer ("Default");
 				}
 			}
 		} else if (hookshot != null && (ZeroGArea != null && canSwitch)) {
 			if (hookshot.transform.parent == null || hookshot.transform.parent == this.gameObject) {
 				if (ZeroGArea.layer == 0) {
+					anim.setAnimation ("Zero G off to on");
 					ZeroGArea.layer = LayerMask.NameToLayer ("Trigger");
 					canSwitch = false;
 				} else if (ZeroGArea.layer == 10) {
+					anim.setAnimation ("zero g on to off");
 					ZeroGArea.layer = LayerMask.NameToLayer ("Default");
 					canSwitch = false;
 				}
